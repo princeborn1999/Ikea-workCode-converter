ebe74b4c5b7f49e8a6cf04ee8dfa09c523cf32c0
Prince <princeborn1999@gmail.com>
2023-08-01 00:28:55 +0800

    fix: logic

--- codeToTime.js
+++ codeToTime.js
@@ -1,23 +1,66 @@
-const code = '0K8'
-function codeToTime(code){
-    let startHour = ''
-    let startMin = ''
-    let endHour = ''
-    let endMin = ''
-    let restTime = ''
-    restTime = turnRestTime(code[code.length - 1])
-    startHour = turnStartHour(String(code[0])+String(code[1]))
-    function turnRestTime(code){
-        if (code === 'H'){
-            return '30'
-        } 
-        return '60'
+const code = "18DH";
+function codeToTime(code) {
+  const restTime = getRestTime(code[code.length - 1]);
+  const startHour = getStartHour(String(code[0]) + String(code[1]));
+  const startMin = getStartMin(code[1]);
+  const workTime = getWorkTime(code);
+  const endTime = getEndTime(workTime, startHour, startMin);
+  function getRestTime(code) {
+    if (code === "H") {
+      return "30";
     }
-    function turnStartHour(code){
-        if(code[0] == '0'){
-            return code.charCodeAt(1) - 64;
-        }
-        return code;
+    return "60";
+  }
+  function getStartHour(code) {
+    if (code[0] == "0") {
+      return code.charCodeAt(1) - 64;
     }
+    return code;
+  }
+  function getStartMin(code) {
+    if (code >= "0" && code <= "9") {
+      return "00";
+    }
+    return "30";
+  }
+  function getWorkTime(code) {
+    let hour = "";
+    let min = "";
+    if (code.length === 4) {
+      // it means code[2]+code[3] === 'X'H
+      if (code[2] > "0" && code[2] < "9") {
+        hour = code[2];
+        min = "00";
+      } else {
+        hour =
+          code[1] > "0" && code[1] < "9"
+            ? code.charCodeAt(2) - 64
+            : code.charCodeAt(2) - 63;
+        min = "30";
+      }
+    } else if (code.length === 3) {
+      if (code[2] === "H") {
+        hour = "9";
+        min = "30";
+      } else {
+        hour = code[2];
+        min = "00";
+      }
+    }
+    return { hour, min };
+  }
+  function getEndTime(workTime, startHour, startMin) {
+    let endMin = parseInt(workTime.min) + parseInt(startMin);
+    let endHour = parseInt(workTime.hour) + parseInt(startHour);
+    if (endMin == 60) {
+      //   endHour += 1;
+      endMin = "00";
+    }
+    endMin = endMin == 0 ? "00" : "30";
+    return { endHour, endMin };
+  }
+  console.log(
+    `start time = ${startHour}:${startMin}, end time = ${endTime.endHour}:${endTime.endMin} ,rest time = ${restTime} min`
+  );
 }
-codeToTime(code)
+codeToTime(code);
